6d4ed4d4f1f7bc55c20383170921f9935984a621
Wei Sun <weisun2013@gmail.com>
2020-07-28 15:13:28 -0700

    update step_x_check_data

--- R/step_x_check_data.R
+++ R/step_x_check_data.R
@@ -1,5 +1,6 @@
 
 library(data.table)
+library(stringr)
 
 # ------------------------------------------------------------------------
 # first check the number of cells per sample for Sade_Feldman_2018
@@ -12,12 +13,38 @@ info.SF = fread(fnm)
 dim(info.SF)
 info.SF[1:2,]
 
+names(info.SF) = gsub("characteristics: ", "", names(info.SF))
+names(info.SF)[5] = "sampleID"
+
+table(info.SF$`molecule`)
+table(info.SF$`description`)
+table(info.SF$`processed data file`)
+table(info.SF$`raw file`)
+
+info.SF = info.SF[,1:7]
+
+pIDs = str_split_fixed(info.SF$sampleID, "_", 2)
+dim(pIDs)
+pIDs[1:2,]
+
+info.SF$patientID = pIDs[,2]
+info.SF$type = pIDs[,1]
+
+dim(info.SF)
+info.SF[1:2,]
+
 length(unique(info.SF$title))
+length(unique(info.SF$patientID))
 
-names(info.SF) = gsub("characteristics: ", "", names(dat))
-names(info.SF)[5] = "patientID"
+info.SF.ind = unique(info.SF[,.(patientID, sampleID, type, response, therapy)])
+dim(info.SF.ind)
+info.SF.ind[1:2,]
 
-sort(table(info.SF$patientID))
+table(info.SF.ind$response, info.SF.ind$therapy)
+table(info.SF.ind$response, info.SF.ind$type)
+
+sort(table(info.SF$sampleID))
+table(tapply(info.SF.ind$type, info.SF.ind$patientID, paste, collapse=":"))
 
 # ------------------------------------------------------------------------
 # Next check the data Jerby_Arnon_2018
@@ -33,11 +60,15 @@ info.JA[1:2,]
 length(unique(info.JA$cells))
 length(unique(info.JA$samples))
 
-sort(table(info.JA$samples))
+# there are 31 individuals, but 33 samples
 
-table(info.JA$Cohort)
 info.JA.ind = unique(info.JA[,.(samples,Cohort,treatment.group)])
 dim(info.JA.ind)
+info.JA.ind = info.JA.ind[order(info.JA.ind$samples),]
+info.JA.ind
+
+
+sort(table(info.JA$samples))
 table(info.JA.ind$Cohort)
 table(info.JA.ind$treatment.group)
 table(info.JA.ind[,.(Cohort,treatment.group)])
